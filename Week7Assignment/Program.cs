2b363674f2091a312cce90b3691b597992e5e1a7
IndigoWendigo <123414451+IndigoWendigo@users.noreply.github.com>
2023-03-09 22:25:13 -0600

    Initial Commit

---
+++ Week7Assignment/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace Week7Assignment
+{
+    internal class Program
+    {
+        static void Main(String[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter a file path: ");
+                string userInput = @"" + Console.ReadLine();
+
+                StreamReader sr = new StreamReader(userInput);
+
+                string line;
+
+                var pathChecker = new Regex(@"^(?:[a - zA - Z]\:|\\\\[\w\.] +\\[\w.$] +)\\(?:[\w] +\\)*\w([\w.])+$");
+
+                do
+                {
+                    Console.WriteLine("Valid file path found:");
+                    line = sr.ReadLine();
+                    Console.WriteLine(line);
+
+                } while (line != null);
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Invalid file path, " + e.Message);
+            }
+        }
+    }
+}
